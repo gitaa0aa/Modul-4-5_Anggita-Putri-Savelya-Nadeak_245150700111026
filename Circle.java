418b78670a28a2831560fb681bacc36efb0ac43f
gitaa0aa <anggitandk@gmail.com>
2025-03-13 15:18:07 +0700

    Circle.java

--- Circle.java
+++ Circle.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package praktic.geometry;
 
-/**
- *
- * @author Hp
- */
 public class Circle {
     private int id;
     private double radius;
